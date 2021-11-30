e12793ac882a49a8f513340d5783c0cacbe197b9
Juveria-Dalvi <juveriadalvi4@gmail.com>
2021-11-30 13:02:30 +0530

    initial commit

--- Extracted Folders/03_05_running_a_script/app2.js
+++ Extracted Folders/03_05_running_a_script/app2.js
@@ -1,1 +1,17 @@
 alert('I AM A DIFFERENT SCRIPT!');
+
+// ----- JavaScript Popup Boxes -----
+
+// JavaScript has three kind of popup boxes:
+// Alert box, Confirm box, and Prompt box.
+alert('Visit this site with caution');
+// return boolean true/false
+
+//  => A confirm box is often used if you want the user to verify or accept something.
+const deleteConfirmation = confirm('are u sure you want to delete this items?');
+// Prompt Box
+const user = prompt('please enter your name?');
+console.log(`user: ${user}`, typeof (user));
+
+var a = prompt('Enter your name', 'NAME');
+document.write(a);
