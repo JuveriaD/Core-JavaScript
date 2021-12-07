486d8ec7ff0905f087277ec3f6129f98e7161200
Juveria-Dalvi <juveriadalvi4@gmail.com>
2021-12-07 17:17:56 +0530

    :)

--- Extracted Folders/07_09_Practice_1_Password_Validator/app.js
+++ Extracted Folders/07_09_Practice_1_Password_Validator/app.js
@@ -10,3 +10,19 @@
 // isValidPassword('89Fjj1nms', 'dogLuvr');  //true
 // isValidPassword('dogLuvr123!', 'dogLuvr') //false
 // isValidPassword('hello1', 'dogLuvr') //false
+
+function isValidPassword(password, username) {
+	if (password.length< 8) {
+		return false;
+	}
+	if (password.indexOf(' ') !== -1) {
+		return false;
+	}
+	if (password.indexOf(username) !== -1) {
+		return false;
+	}
+	return true;
+	
+}
+console.log(isValidPassword("password123","my name"));//true
+console.log(isValidPassword("password 123","jhfswgsfgvfdvxcv"));//false
