1620fb6134066a3571a96246d45cf4f0f41d6929
Juveria-Dalvi <juveriadalvi4@gmail.com>
2021-12-09 16:37:29 +0530

    :)

--- Extracted Folders/07_10_Practice_2_Avg/app.js
+++ Extracted Folders/07_10_Practice_2_Avg/app.js
@@ -1,3 +1,15 @@
 // Write a function to find the average value in an array of numbers
-//avg([0,50]) //25
+// average([0,50]) //25
 //avg([75,76,80,95,100]) //85.2
+function average(arr){
+    let total=0;
+//loop over each num
+for (let i = 0; i < arr.length; i++) {
+    const num = arr[i];
+    //add them together
+    total += num;
+}
+//divide by length of arr
+let result = total/arr.length;
+return result;
+}
