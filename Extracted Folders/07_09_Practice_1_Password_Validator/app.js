d6a193be222bbe87f3f589ee45c2e825636e1307
Juveria-Dalvi <juveriadalvi4@gmail.com>
2021-12-09 13:25:16 +0530

    :)

--- Extracted Folders/07_09_Practice_1_Password_Validator/app.js
+++ Extracted Folders/07_09_Practice_1_Password_Validator/app.js
@@ -27,12 +27,33 @@
 //console.log(isValidPassword("password123","my name"));//true
 //console.log(isValidPassword("password 123","jhfswgsfgvfdvxcv"));//false
 
+// function isValidPassword(password,username){
+// if(password.length < 8 || password.indexOf(' ') !== -1){
+// return false;
+// }
+// if(password.indexOf(username) !== -1){
+// 	return false;
+// }
+// return true;
 
-function isValidPassword(password,username){
-if(password.length <= 8 && password.indexOf(' ') == -1 && password.indexOf(username))
-{
-return false;
-}
-return true;
+// }
 
+function isValidPassword(password, username) {
+	username = prompt('Enter username');
+	password = prompt('Enter password!');
+	let res = 'Invalid Password OR UserName';
+	if(username == null && password == null){
+		document.write(res)
+	}
+	if(username.indexOf(password) !== -1){
+		document.write(res, " <br>")
+	}
+	if ( password.length < 8 || password.indexOf(' ') !== -1) {
+		document.write(res);
+	}
+	else {
+		document.write('<br>' +'UserName :' + username + '<br>' + 
+		'Password :' + password);
+	}
 }
+isValidPassword();
